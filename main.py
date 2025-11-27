d7ee50117132c46922e76eee8331e750f702e6ce
Bibindum <aleksandrsluginov77@gmail.com>
2025-11-27 15:15:41 +0300

    add table

--- main.py
+++ main.py
@@ -1,1 +1,31 @@
-print("Hello World")
+import psycopg2
+
+CONNECT_DB = {
+    "host": "localhost",
+    "user": "postgres",
+    "password": "admin",
+    "dbname": "postgres1",
+    "port": "5432"
+}
+
+def init_database():
+    with psycopg2.connect(**CONNECT_DB) as conn:
+        with conn.cursor() as cursor:
+            cursor.execute("""
+                           CREATE TABLE IF NOT EXISTS tasks
+                           (
+                               id
+                               SERIAL
+                               PRIMARY
+                               KEY,
+                               title
+                               TEXT
+                               NOT
+                               NULL,
+                               priority
+                               TEXT
+                               NOT
+                               NULL
+                           )
+                           """)
+        conn.commit()
